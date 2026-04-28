a8d5ba10f029821f96fc1b72989e543728ba69d9
DhaanyaPrakash <230701069@rajalakshmi.edu.in>
2026-04-28 22:40:34 +0530

    update frontend api url

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-export const axiosi=axios.create({withCredentials:true,baseURL:process.env.REACT_APP_BASE_URL})
+export const axiosi=axios.create({withCredentials:true,baseURL:"https://dhaanya-backend-1-g4bwbfapffcxathf.southeastasia-01.azurewebsites.net"})
