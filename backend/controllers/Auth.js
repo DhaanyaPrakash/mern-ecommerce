2821e1bcb977ce4514098551e35f689ab761cd2d
DhaanyaPrakash <230701069@rajalakshmi.edu.in>
2026-04-29 10:37:22 +0530

    change maxAge

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -33,7 +33,7 @@ exports.signup=async(req,res)=>{
         // sending jwt token in the response cookies
         res.cookie('token',token,{
             sameSite:process.env.PRODUCTION==='true'?"None":'Lax',
-            maxAge:new Date(Date.now() + (parseInt(process.env.COOKIE_EXPIRATION_DAYS * 24 * 60 * 60 * 1000))),
+            maxAge:parseInt(process.env.COOKIE_EXPIRATION_DAYS * 24 * 60 * 60 * 1000),
             httpOnly:true,
             secure:process.env.PRODUCTION==='true'?true:false
         })
@@ -63,7 +63,7 @@ exports.login=async(req,res)=>{
             // sending jwt token in the response cookies
             res.cookie('token',token,{
                 sameSite:process.env.PRODUCTION==='true'?"None":'Lax',
-                maxAge:new Date(Date.now() + (parseInt(process.env.COOKIE_EXPIRATION_DAYS * 24 * 60 * 60 * 1000))),
+                maxAge:parseInt(process.env.COOKIE_EXPIRATION_DAYS * 24 * 60 * 60 * 1000),
                 httpOnly:true,
                 secure:process.env.PRODUCTION==='true'?true:false
             })
