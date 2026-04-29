cb89428dd53883a5ad6ef928025ac2284b669e7d
DhaanyaPrakash <230701069@rajalakshmi.edu.in>
2026-04-29 10:13:12 +0530

    force fix jwt expiry

--- backend/utils/GenerateToken.js
+++ backend/utils/GenerateToken.js
@@ -3,10 +3,9 @@ const jwt = require('jsonwebtoken')
 
 exports.generateToken = (payload, passwordReset = false) => {
 
-    const loginExpiry = process.env.LOGIN_TOKEN_EXPIRATION || "30d";
-    const resetExpiry = process.env.PASSWORD_RESET_TOKEN_EXPIRATION || "2m";
+    const expiry = passwordReset ? "2m" : "30d";
 
-    return jwt.sign(payload, process.env.SECRET_KEY, {
-        expiresIn: passwordReset ? resetExpiry : loginExpiry
+    return jwt.sign(payload, process.env.SECRET_KEY || "mysecret123", {
+        expiresIn: expiry
     });
 }
