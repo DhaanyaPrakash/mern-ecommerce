a40485b2e12fc0a4496eb3ef21a97e77d8f49a10
DhaanyaPrakash <230701069@rajalakshmi.edu.in>
2026-04-29 10:02:37 +0530

    fix jwt expiry properly

--- backend/utils/GenerateToken.js
+++ backend/utils/GenerateToken.js
@@ -1,6 +1,12 @@
 require('dotenv').config()
-const jwt=require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
-exports.generateToken=(payload,passwordReset=false)=>{
-    return jwt.sign(payload,process.env.SECRET_KEY,{expiresIn:passwordReset?process.env.PASSWORD_RESET_TOKEN_EXPIRATION:process.env.LOGIN_TOKEN_EXPIRATION || "30d"})
+exports.generateToken = (payload, passwordReset = false) => {
+
+    const loginExpiry = process.env.LOGIN_TOKEN_EXPIRATION || "30d";
+    const resetExpiry = process.env.PASSWORD_RESET_TOKEN_EXPIRATION || "2m";
+
+    return jwt.sign(payload, process.env.SECRET_KEY, {
+        expiresIn: passwordReset ? resetExpiry : loginExpiry
+    });
 }
